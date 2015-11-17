26e6c5629d2374944e6ec8f7956c017fcb9f603f
Mahmudbek Odilov <modilov@dealerfire.com>
2015-11-17 10:26:55 +0500

    bootstrap and jquery added

--- config/frontend.php
+++ config/frontend.php
@@ -16,10 +16,14 @@ return array_merge(
 				/*
 				'src' => '',
 				'type' => 'script' // script, style, or array of tag params
+				'code' => {code of style or js}
 				*/
+				'bootstrap' => array('src' => 'bootstrap.min.css', 'type' => 'style'),
+				'bootstrap-theme' => array('src' => 'bootstrap-theme.min.css', 'type' => 'style')
 			),
 			'footer' => array(
-				'jquery' => array('src' => '', 'type' => 'script')
+				'jquery' => array('src' => 'jquery-1.11.3.min.js', 'type' => 'script'),
+				'bootstrap' => array('src' => 'bootstrap.min.js', 'type' => 'script')
 			)
 		)
 	)
