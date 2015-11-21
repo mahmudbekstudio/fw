6cdd4723e8358d9ae3a32ec39aaeedb08b29eea3
Mahmudbek Odilov <modilov@dealerfire.com>
2015-11-21 17:43:05 +0500

    render timer

--- frontend/index.php
+++ frontend/index.php
@@ -1,4 +1,14 @@
 <?php
-define('APPENV', 'frontend');
+function microtime_float()
+{
+	list($usec, $sec) = explode(" ", microtime());
+	return ((float)$usec + (float)$sec);
+}
+$time_start = microtime_float();
 
+define('APPENV', 'frontend');
 require '../settings.php';
+
+$time_end = microtime_float();
+$time = round(($time_end - $time_start) * 10000) / 10000;
+echo "<p align='center'>$time seconds</p>";
